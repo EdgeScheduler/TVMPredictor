93963a64b8fbaeeac51f2199931c86a214e3a5f3
oneflyingfish <a_flying_fish@outlook.com>
2021-10-12 07:59:45 +0000

    update

--- create_dataset/test_code/op_test_code/start_test.sh
+++ create_dataset/test_code/op_test_code/start_test.sh
@@ -8,11 +8,10 @@ echo "---start to test all operators---"
 
 index=1
 COUNT=`find $SCRIPTFOLD/run/ -type f | wc -l`
-echo "$COUNT"
 for data in $SCRIPTFOLD/run/*
 do
     echo "start to run($index/$COUNT): python3 -u \"$data\""
-    # python3 -u \"$data\"
+    python3 $data
     echo -e "done($index/$COUNT): python3 -u \"$data\".\n"
     index=`expr $index + 1`
 done 
