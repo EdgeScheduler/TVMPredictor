a0207d584b3028c4634f4523078ce1d5b646a4b3
oneflyingfish <a_flying_fish@outlook.com>
2021-11-12 03:44:29 -0600

    upload

--- create_dataset/test_code/model_test_code/config/common_args.py
+++ create_dataset/test_code/model_test_code/config/common_args.py
@@ -1,16 +1,17 @@
 from create_dataset.common import Device
 
 min_shapes=1
-max_shapes=100
+max_shapes=128
 sampling=1.0
 dtype="float32"
 cycle_times=3
-min_repeat_ms=30
+min_repeat_ms=10
 opt_level=0
 fold_path="create_dataset/datasets_models/"
 show_print=True
 isModel = True
 device_name="dell04"
+config_name = "dataset_"+device_name+".json"
 
 # device_parame_array = [Device.device_params_CPU,Device.device_params_GPU0]
 device_parame_array = [Device.device_params_GPU1,]
