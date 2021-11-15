8b9d6b8946265d80c20089e3f43ba09893264666
oneflyingfish <a_flying_fish@outlook.com>
2021-11-15 03:30:09 -0600

    update

--- create_dataset/datasets_model/merge.sh
+++ create_dataset/datasets_model/merge.sh
@@ -14,15 +14,15 @@ COUNT=`find $jsons_path -type f | wc -l`
 for data in $jsons_path
 do
     if [ "$data" == "$aim_path" ];then
-        echo "done($index/$COUNT): skip merge $aim_path with itself."
+        echo -e "done($index/$COUNT): skip merge $aim_path with itself.\n"
         index=`expr $index + 1`
         continue
     fi
 
-    echo "start to run($index/$COUNT): python3 -u \"$SCRIPTFOLD/../../create_dataset/fix_json/merge_dataset_files.py\" $aim_path $data"
-    python3 -u "$SCRIPTFOLD/../../create_dataset/fix_json/merge_dataset_files.py" $aim_path $data
+    echo "start to run($index/$COUNT): python3 -u $SCRIPTFOLD/../../create_dataset/fix_json/merge_dataset_files.py $aim_path $data"
+    python3 -u $SCRIPTFOLD/../../create_dataset/fix_json/merge_dataset_files.py $aim_path $data
     
-    echo -e "done($index/$COUNT): python3 -u \"$data\".\n"
+    echo -e "done($index/$COUNT).\n"
     index=`expr $index + 1`
 done 
 echo "---finished---"
