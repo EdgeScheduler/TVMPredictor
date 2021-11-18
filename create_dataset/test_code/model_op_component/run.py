ee89afcba064e6b6b77733c155f2460a50b1da96
oneflyingfish <a_flying_fish@outlook.com>
2021-11-18 10:57:47 -0600

    update

--- create_dataset/test_code/model_op_component/run.py
+++ create_dataset/test_code/model_op_component/run.py
@@ -1,10 +1,12 @@
+from multiprocessing.context import Process
 import os
 import json
 import ast
 from TVMProfiler.model_src.analyze_componnet import get_op_info
 import tvm
 import traceback
-import threading
+import multiprocessing
+from multiprocessing import Pool
 class SaveInfo:
     fold_path="Datasets/TVM/models_component/"
     config_name = "dataset.json"
@@ -14,8 +16,6 @@ class ModelsRuntimeInfo:
     prefix_fold = "Datasets/"
     fold_path="TVM/datasets_models/"
     config_name = "dataset_auto.json"
-
-TVM_INIT=False
 
 def add_model_component(model_name,shape,batch_size,component_dict,dataset_name="dataset.json",prefix_dataset_name="",fold_path="",auto_skip=True)->dict:
     dataset_name = prefix_dataset_name + dataset_name
@@ -106,82 +106,58 @@ def analyze_models_by_runtime_json(deal_function,object_names=[],prefix_dataset_
 
                         if print_info:
                             print("\n")
-                            print("device name",device_name)
-                            print("device id",device_id)
+                            print("\n\noriginal device name",device_name)
+                            print("original device id",device_id)
                             print("object name: ",object_name)
                             print("shape", shape)
-                            # print("batch size(doing):   ",end="")
 
-                        component_add_list={}
-                        threads=[]
+                        run_values=[]
+                        cores = multiprocessing.cpu_count()
+                        pool = Pool(processes=cores*2)
 
                         value = runtime_config[device_name][object_name][device_id][shapes_dimensionality][shape]
                         with open(os.path.join(ModelsRuntimeInfo.prefix_fold, value["file_path"]),"r") as f:
                             line = f.readline()
                             while line is not None and len(line)>0 :
                                 batch_size = int(line.split(",")[0])
-                                # if print_info:
-                                    # pass
-                                    # print("\b\b\b%-3d"%batch_size,end="")
-
-                                # runtime = float(line.split(",")[1])
 
                                 dshapes = ast.literal_eval(shape.replace("-1",str(batch_size)))
 
                                 if not exist_in_dataset(model_name=object_name,shape=shape,batch_size=batch_size,dataset_name=SaveInfo.config_name,fold_path=SaveInfo.fold_path,prefix_dataset_name=prefix_dataset_name):
-                                    thd = threading.Thread(target=analyze_model_component,args=(object_name,dshapes,batch_size,component_add_list,print_info),daemon=True)
-                                    threads.append(thd)
-                                    
-                                    # flag,model_component_dict = deal_function(object_name,dshapes,shape=shape)
-
-                                    # if not flag:
-                                    #     # print("analyze model component error: ",error_info)
-                                    #     return False
-
-                                    if not TVM_INIT and print_info:
-                                        TVM_INIT = True
-                                        print("\n\noriginal device name",device_name)
-                                        print("original device id",device_id)
-                                        print("object name: ",object_name)
-                                        print("shape", shape)
-                                        # print("batch size(doing):   ",end="")
-
-                                    # component_add_list[batch_size]=model_component_dict
-
-                                    print("arrange batch_size=%d to thead: (name: %s, id: %d)."%(batch_size,thd.getName(),thd.ident))
-
-                                    # add_model_component(model_name=object_name,shape=shape,batch_size=batch_size,component_dict=model_component_dict,prefix_dataset_name=prefix_dataset_name,dataset_name=SaveInfo.config_name,fold_path=SaveInfo.fold_path,auto_skip=SaveInfo.auto_skip)
-
+                                    run_process=pool.apply_async(func=analyze_model_component,args=(object_name,dshapes,batch_size,print_info))
+                                    run_values.append((batch_size,run_process))                                   
                                 line = f.readline()
-                        
-                        for t in threads:
-                            t.start()
-                            t.join()        # 同步
+                            
+                        pool.close()        # 使其不再接受新的任务
+                        pool.join()         # 必须在close/terminate后面执行，等待所有子进程结束
 
                         if print_info:
                             print("finish calculate all batch size.")
-                            print("writing batch size:   ")
-                            
-                        for batch_size_k,component_dict_v in component_add_list:
-                            print("\b\b\b%-3d"%batch_size)
-                            add_model_component(model_name=object_name,shape=shape,batch_size=batch_size_k,component_dict=component_dict_v,prefix_dataset_name=prefix_dataset_name,dataset_name=SaveInfo.config_name,fold_path=SaveInfo.fold_path,auto_skip=SaveInfo.auto_skip)
 
+                        for value in run_values:
+                            add_model_component(model_name=object_name,shape=shape,batch_size=value[0],component_dict=value[1].get(),prefix_dataset_name=prefix_dataset_name,dataset_name=SaveInfo.config_name,fold_path=SaveInfo.fold_path,auto_skip=SaveInfo.auto_skip)
+
+                        if print_info:    
+                            print("finish record.")
     return True
 
 # 待实现
-def analyze_model_component(model_name,dshape,batch_size,component_add_list,print_info=True,dtype="float32"):
+def analyze_model_component(model_name,dshape,batch_size,print_info=True,dtype="float32"):
     try:
+        print("arrange batch_size=%d to process: PID=%d"%(batch_size,os.getpid()))
         device = tvm.cpu(0)
         target = "llvm"
-        component_add_list[batch_size] = get_op_info(model_name,dshape[0],device=device,target=target)
+        component_dict = get_op_info(model_name,dshape[0],device=device,target=target)
         if print_info:
-            print("%s finish work for batch_siz=%d."%(threading.current_thread().name,batch_size))
-    except:
-        traceback.print_exc()
-        exit(-1)
+            print("finish analyze for batch_size=%d."%(batch_size))
+        return component_dict
+    except Exception as ex:
+        # traceback.print_exc()
+        return {"error",str(ex)}
 
 def main():
-    if analyze_models_by_runtime_json(analyze_model_component,object_names=["inception_v3","mobilenet","resnet-50","resnet3d-50","squeezenet_v1.1"],print_info=True):
+    print("<%d cores in your device.>"%multiprocessing.cpu_count())
+    if analyze_models_by_runtime_json(analyze_model_component,object_names=["mobilenet","resnet-50","resnet3d-50","squeezenet_v1.1"],print_info=True):
         print("finish all works.")
     else:
         print("break off...")
--- create_dataset/test_code/model_test_code/run/test_inception_v3.py
+++ create_dataset/test_code/model_test_code/run/test_inception_v3.py
@@ -1,5 +1,5 @@
 from create_dataset.test_code.model_test_code.operation_statistics.cnn_workload_generator import get_network
-from create_dataset.common import generate_datasets_with_one_dimensionality_changing,uniform_sampling,generate_dataset_with_one_dimensionality_changing
+from create_dataset.common import generate_datasets_with_one_dimensionality_changing,uniform_sampling,generate_dataset_with_one_dimensionality_changing,Device
 import tvm.relay as relay
 import create_dataset.test_code.model_test_code.config.common_args as common_args
 
@@ -24,14 +24,14 @@ force_shape_relation=(None,)
 shapes_dimensionality=((4,),(0,0))
 
 count = 1
-range_min = [[-1,3,16,16],]
-range_max = [[-1,3,256,256],]
-for a in uniform_sampling(16,256,0.0625):
-    for b in uniform_sampling(16,256,0.0625):
+range_min = [[-1,3,299,299],]
+range_max = [[-1,3,299,299],]
+for a in uniform_sampling(299,299,0.0625):
+    for b in uniform_sampling(299,299,0.0625):
         range_min[0][2]=a
         range_max[0][2]=a
         range_min[0][3]=b
         range_max[0][3]=b
 
-        generate_datasets_with_one_dimensionality_changing(device_parame_array=common_args.device_parame_array,count=count,shape_dimensionality=shapes_dimensionality,range_min=range_min,range_max=range_max,function_dict = function_dict,min_shapes=common_args.min_shapes,max_shapes=common_args.max_shapes,sampling=common_args.sampling,force_shape_relation=force_shape_relation,dtype=common_args.dtype,cycle_times=common_args.cycle_times,min_repeat_ms=common_args.min_repeat_ms,opt_level=common_args.opt_level,prefix_path=common_args.prefix_path,fold_path=common_args.fold_path,device_name=common_args.device_name,show_print=common_args.show_print,isModule=common_args.isModel,dataset_config_name=common_args.config_name)
+        generate_datasets_with_one_dimensionality_changing(device_parame_array=[Device.device_params_GPU1,],count=count,shape_dimensionality=shapes_dimensionality,range_min=range_min,range_max=range_max,function_dict = function_dict,min_shapes=common_args.min_shapes,max_shapes=common_args.max_shapes,sampling=common_args.sampling,force_shape_relation=force_shape_relation,dtype=common_args.dtype,cycle_times=common_args.cycle_times,min_repeat_ms=common_args.min_repeat_ms,opt_level=common_args.opt_level,prefix_path=common_args.prefix_path,fold_path=common_args.fold_path,device_name=common_args.device_name,show_print=common_args.show_print,isModule=common_args.isModel,dataset_config_name=common_args.config_name)
         count+=1
