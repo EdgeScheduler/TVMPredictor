aed2dabe39b2107c3ba86fbf3ba2e2c10597b35b
Xuyuanjia2014 <xuyuanjia2017@otcaix.iscas.ac.cn>
2022-02-15 17:13:43 +0800

    1

---
+++ create_dataset/test_code/op_test_code/test_run.py
@@ -0,0 +1,7 @@
+import os
+cpu_num_list = [1,2,3,4,5]
+for root,dirs,files in os.walk('run/'):
+    for f in files:
+        print(os.path.join(root,f))
+# for cpu_num in cpu_num_list:
+#     os.system
